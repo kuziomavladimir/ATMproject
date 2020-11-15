e113c69489e6755c75fca5690259fe08d81ffc91
kuziomavladimir <kuziomavladimir@yandex.ru>
2020-11-15 23:03:06 +0300

    add tests

---
+++ src/test/java/cardpackage/CardTest.java
@@ -0,0 +1,13 @@
+package cardpackage;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CardTest {
+
+    @Test
+    void getBalance() {
+        assertEquals(1,1);
+    }
+}
