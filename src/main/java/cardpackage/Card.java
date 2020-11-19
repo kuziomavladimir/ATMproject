2c47be7a42a8b7dbdaeb2d0fcc31757ec1080bf2
kuziomavladimir <kuziomavladimir@yandex.ru>
2020-11-19 16:48:40 +0300

    add lombok

--- src/main/java/cardpackage/Card.java
+++ src/main/java/cardpackage/Card.java
@@ -1,5 +1,10 @@
 package cardpackage;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Card {
     private String cardNumber;
     private String pinCode;
@@ -9,37 +14,5 @@ public class Card {
     public Card(String cardNumber, String pinCode) {
         this.cardNumber = cardNumber;
         this.pinCode = pinCode;
-    }
-
-    public String getCardNumber() {
-        return cardNumber;
-    }
-
-    public void setCardNumber(String cardNumber) {
-        this.cardNumber = cardNumber;
-    }
-
-    public String getPinCode() {
-        return pinCode;
-    }
-
-    public void setPinCode(String pinCode) {
-        this.pinCode = pinCode;
-    }
-
-    public double getBalance() {
-        return balance;
-    }
-
-    public void setBalance(double balance) {
-        this.balance = balance;
-    }
-
-    public String getAmount() {
-        return amount;
-    }
-
-    public void setAmount(String amount) {
-        this.amount = amount;
     }
 }
